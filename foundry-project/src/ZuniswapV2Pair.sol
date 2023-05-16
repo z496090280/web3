192b17873e589c8a57ed24bbfe1c16b3d96752d5
daping.li <daping.li@yiwill.com>
2023-05-16 19:36:37 +0800

    feat: add foundry

---
+++ foundry-project/src/ZuniswapV2Pair.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./libraries/Math.sol";
+import "./solmate/tokens/ERC20.sol"
+
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+
+    function transfer(address to, uint256 amount) external;
+}
+
+error InsufficientLiquidityMinted();
+error InsufficientLiquidityBurned();
+error TransferFailed();
+
+contract ZuniswapV2Pair is ERC20, Math {
+  uint256 constant MINIMUM_LIQUIDITY = 1000;
+  address public token0;
+  address public token1;
+
+  uint112 private reserve0;
+  uint112 private reserve1;
+
+  event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+  event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+  event Sync(uint256 reserve0, uint256 reserve1);
+
+  constructor(address token0_, address token1_) ERC20("ZuniswapV2 Pair", "ZUNIV2", 18) {
+    token0 = token0_;
+    token1 = token1_;
+  }
+
+  function mint() public {
+    (uint256 _reserve0, uint112 _reserve1) = getReserves();
+    uint256 balance0 = IERC20(token0).balanceOf(address(this));
+    uint256 balance1 = IERC20(token1).balanceOf(address(this));
+    // 计算资产流通差额
+    uint256 amount0 = balance0 - reserve0;
+    uint256 amount1 = balance1 - reserve1;
+
+    uint256 liquidity;
+
+    if(totalSupply == 0) {
+      liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+      _mint(address(0), MINIMUM_LIQUIDITY);
+    } else {
+      liquidity = Math.min(
+        totalSupply * amount0 / _reserve0,
+        totalSupply * amount1 / _reserve1
+      );
+    }
+
+    if (liquidity <= 0) revert InsufficientLiquidityMinted();
+
+
+    _mint(msg.sender, liquidity);
+
+    _update(balance0, balance1);
+
+    emit Mint(msg.sender, amount0, amount1);
+  }
+
+  function getReserves() public view returns (uint112, uint112, uint32) {
+    return (
+      reserve0, reserve1, 0
+    );
+  }
+
+  function _update(uint256 balance0, uint256 balance1) private {
+    reserve0 = uint112(balance0);
+    reserve1 = uint112(balance1);
+
+    emit Sync(reserve0, reserve1);
+  }
+}
