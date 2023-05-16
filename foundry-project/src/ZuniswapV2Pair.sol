f9eb3ddd71367b4520aea7b779c07109fe0d8702
daping.li <daping.li@yiwill.com>
2023-05-16 23:09:07 +0800

    feat: 编写pair测试用例

--- foundry-project/src/ZuniswapV2Pair.sol
+++ foundry-project/src/ZuniswapV2Pair.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "./libraries/Math.sol";
-import "./solmate/tokens/ERC20.sol"
+import "./solmate/tokens/ERC20.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -14,63 +14,62 @@ error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
 error TransferFailed();
 
-contract ZuniswapV2Pair is ERC20, Math {
-  uint256 constant MINIMUM_LIQUIDITY = 1000;
-  address public token0;
-  address public token1;
+contract ZuniswapV2Pair is ERC20 {
+    uint256 constant MINIMUM_LIQUIDITY = 1000;
+    address public token0;
+    address public token1;
 
-  uint112 private reserve0;
-  uint112 private reserve1;
+    uint112 private reserve0;
+    uint112 private reserve1;
 
-  event Burn(address indexed sender, uint256 amount0, uint256 amount1);
-  event Mint(address indexed sender, uint256 amount0, uint256 amount1);
-  event Sync(uint256 reserve0, uint256 reserve1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Sync(uint256 reserve0, uint256 reserve1);
 
-  constructor(address token0_, address token1_) ERC20("ZuniswapV2 Pair", "ZUNIV2", 18) {
-    token0 = token0_;
-    token1 = token1_;
-  }
-
-  function mint() public {
-    (uint256 _reserve0, uint112 _reserve1) = getReserves();
-    uint256 balance0 = IERC20(token0).balanceOf(address(this));
-    uint256 balance1 = IERC20(token1).balanceOf(address(this));
-    // 计算资产流通差额
-    uint256 amount0 = balance0 - reserve0;
-    uint256 amount1 = balance1 - reserve1;
-
-    uint256 liquidity;
-
-    if(totalSupply == 0) {
-      liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
-      _mint(address(0), MINIMUM_LIQUIDITY);
-    } else {
-      liquidity = Math.min(
-        totalSupply * amount0 / _reserve0,
-        totalSupply * amount1 / _reserve1
-      );
+    constructor(address token0_, address token1_)
+        ERC20("ZuniswapV2 Pair", "ZUNIV2", 18)
+    {
+        token0 = token0_;
+        token1 = token1_;
     }
 
-    if (liquidity <= 0) revert InsufficientLiquidityMinted();
+    function mint() public {
+        (uint112 _reserve0, uint112 _reserve1) = getReserves();
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        // 计算资产流通差额
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
 
+        uint256 liquidity;
 
-    _mint(msg.sender, liquidity);
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = Math.min(
+                (totalSupply * amount0) / _reserve0,
+                (totalSupply * amount1) / _reserve1
+            );
+        }
 
-    _update(balance0, balance1);
+        if (liquidity <= 0) revert InsufficientLiquidityMinted();
 
-    emit Mint(msg.sender, amount0, amount1);
-  }
+        _mint(msg.sender, liquidity);
 
-  function getReserves() public view returns (uint112, uint112, uint32) {
-    return (
-      reserve0, reserve1, 0
-    );
-  }
+        _update(balance0, balance1);
 
-  function _update(uint256 balance0, uint256 balance1) private {
-    reserve0 = uint112(balance0);
-    reserve1 = uint112(balance1);
+        emit Mint(msg.sender, amount0, amount1);
+    }
 
-    emit Sync(reserve0, reserve1);
-  }
+    function getReserves() public view returns (uint112, uint112) {
+        return (reserve0, reserve1);
+    }
+
+    function _update(uint256 balance0, uint256 balance1) private {
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+
+        emit Sync(reserve0, reserve1);
+    }
 }
---
+++ foundry-project/test/ZuniswapV2Pair.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/ZuniswapV2Pair.sol";
+import "../src/solmate/tokens/ERC20.sol";
+
+contract ERC20Mintable is ERC20 {
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol, 18)
+    {}
+
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
+    }
+}
+
+contract ZuniswapV2PairTest is Test {
+    ZuniswapV2Pair pair;
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+
+    function setUp() public {
+        token0 = new ERC20Mintable("TokenA", "TKNA");
+        token1 = new ERC20Mintable("TokenB", "TKNB");
+        pair = new ZuniswapV2Pair(address(token0), address(token1));
+
+        token0.mint(10 ether);
+        token1.mint(10 ether);
+    }
+
+    function testMintBootstrap() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+        assertReserves(1 ether, 1 ether);
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        assertEq(pair.totalSupply(), 1 ether);
+    }
+
+    function testMintLiquidity() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // +1 LP
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 2 ether);
+
+        pair.mint(); // +2 LP
+
+        assertEq(pair.balanceOf(address(this)), 3 ether - 1000);
+        assertReserves(3 ether, 3 ether);
+        assertEq(pair.totalSupply(), 3 ether);
+    }
+
+    // 测试不安正常比例
+    function testMintUnbalanced() public {
+        // 这里 1 / 2 ether 都将得到 1 ether的奖励
+        token0.transfer(address(pair), 1 ether);
+        // token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(); // +1 LP
+
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        // assertReserves(1 ether, 1 ether);
+        // assertEq(pair.totalSupply(), 1 ether);
+    }
+
+    function assertReserves(uint112 reserve0, uint112 reserve1) public {
+        (uint112 _reserve0, uint112 _reserve1) = pair.getReserves();
+        assertEq(_reserve0, reserve0, "_reserve0");
+        assertEq(_reserve1, reserve1, "_reserve1");
+    }
+}
