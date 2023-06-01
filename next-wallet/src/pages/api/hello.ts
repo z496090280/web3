26c1166f30af4ce057d4b1ea05d628ff89db71e5
lee <496090280@qq.com>
2023-06-01 23:20:01 +0800

    fix: eslint

--- next-wallet/src/pages/api/hello.ts
+++ next-wallet/src/pages/api/hello.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: lee
  * @Date: 2023-06-01 16:42:31
- * @LastEditTime: 2023-06-01 16:42:38
+ * @LastEditTime: 2023-06-01 23:19:35
  */
-export default (req, res) => {
+export default (req: any, res: any) => {
   res.status(200).json({ text: "Hello" });
 };
