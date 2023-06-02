8c51c8f4bd050057df0b7f3c3d1820591f79f11c
lee <496090280@qq.com>
2023-06-02 11:34:22 +0800

    feat: next config

--- next-wallet/next.config.js
+++ next-wallet/next.config.js
@@ -1,13 +1,14 @@
 /*
  * @Author: lee
  * @Date: 2023-05-08 18:08:15
- * @LastEditTime: 2023-06-01 23:36:57
+ * @LastEditTime: 2023-06-02 11:31:03
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
     forceSwcTransforms: true,
+    appDir: false,
   },
 }
 
